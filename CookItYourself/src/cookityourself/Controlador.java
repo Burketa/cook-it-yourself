97e1f1f782e96c0438915a490285e113184f5f6b
burca <rlpelizzon@hotmail.com>
2019-05-30 22:30:18 -0300

    Segundo commit teste

---
+++ CookItYourself/src/cookityourself/Controlador.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cookityourself;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Aluno
+ */
+public class Controlador {
+    private ArrayList<Receita> receitasCandidatas;
+    private ArrayList<Ingrediente> ingredientesIncluidos;
+    private ArrayList<Ingrediente> ingredientesRestritos;
+    
+}
