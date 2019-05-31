8b8c19951fbd6bd1ee78abf0f4c1cf69df73ee62
burca <rlpelizzon@hotmail.com>
2019-05-30 22:40:16 -0300

    Terceiro Teste Commit

--- CookItYourself/src/cookityourself/Controlador.java
+++ CookItYourself/src/cookityourself/Controlador.java
@@ -15,5 +15,30 @@ public class Controlador {
     private ArrayList<Receita> receitasCandidatas;
     private ArrayList<Ingrediente> ingredientesIncluidos;
     private ArrayList<Ingrediente> ingredientesRestritos;
+    private ArrayList<Integer> preferenciasEscolhidas;
     
+    public ArrayList<Receita> incluirIngrediente(ArrayList<Ingrediente> ingredientes)
+    {
+        return new ArrayList<Receita>();
+    }
+    
+    public ArrayList<Receita> restringirIngrediente(ArrayList<Ingrediente> ingredientes)
+    {
+        return new ArrayList<Receita>();
+    }
+    
+    public ArrayList<Receita> filtrar(int categoria, int cozinhaTipica,ArrayList<Receita> receitasCandidatas)
+    {
+        return new ArrayList<Receita>();
+    }
+    
+    public boolean compartilharEmail()
+    {
+        return false;
+    }
+    
+    public boolean compartilharWhatsapp()
+    {
+        return false;
+    }
 }
